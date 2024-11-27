70dfcd8111d93cd8b5277f943d558128120a03c5
Orhan Veli <orhanvelicelik002@gmail.com>
2024-11-27 17:38:27 +0300

    Changes to be committed: 	modified:   .vscode/c_cpp_properties.json 	modified:   .vscode/settings.json 	modified:   Sending_Massage_via_UART/Sending_Message_via_UART.c

--- Sending_Massage_via_UART/Sending_Message_via_UART.c
+++ Sending_Massage_via_UART/Sending_Message_via_UART.c
@@ -5,10 +5,28 @@
 
 int main (void){
 
+    //USART2 TX Pin (PA2) Config
+    GPIO_InitTypeDef GPIO_InitStruct_A2;
+	GPIO_InitStruct_A2.GPIO_Pin=GPIO_Pin_2;
+	GPIO_InitStruct_A2.GPIO_Mode=GPIO_Mode_AF_PP;
+	GPIO_InitStruct_A2.GPIO_Speed=GPIO_Speed_50MHz;
+
+    //USART2 RX Pin (PA3) Config
+    GPIO_InitTypeDef GPIO_InitStruct_A3;
+	GPIO_InitStruct_A3.GPIO_Pin=GPIO_Pin_3;
+	GPIO_InitStruct_A3.GPIO_Mode=GPIO_Mode_IPU;
+
+
+     //Enable USART2
+    RCC_APB1PeriphClockCmd(RCC_APB1ENR_USART2EN, ENABLE);
+
+    GPIO_DeInit(GPIOA);
+    //Functions to initialize USART2 at PA3 as RX and PA2 as TX
+    GPIO_Init(GPIOA,&GPIO_InitStruct_A3);
+    GPIO_Init(GPIOA,&GPIO_InitStruct_A2);
+
     
-
-
-
+    
     while (1){
 
     }
